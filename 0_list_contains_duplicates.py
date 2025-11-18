3e3002ef3e88aa8352d2cc57eafba6373bd85d5c
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-11-18 09:49:26 -0600

    Update 0_list_contains_duplicates.py

--- 0_list_contains_duplicates.py
+++ 0_list_contains_duplicates.py
@@ -12,3 +12,16 @@ def list_contains_duplicates(lst):
     return False
 
 print(list_contains_duplicates([1,2,3]))
+
+
+# 
+def list_contains_duplicates(lst):
+
+    # convert the lst into a set, which doesn't have duplidates
+    new_lst = set(lst)
+
+    # compare the length of the original lst and the new_lst, if they're the same then the lst doesn't containe duplicates
+    # make sure to do the oppposite, since True = duplicates
+    return len(new_lst) != len(lst)
+
+print(list_contains_duplicates([1,2,3,3,4]))
