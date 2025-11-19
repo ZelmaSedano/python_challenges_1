a35315c04466365eebe0a42d1f3a291f11579929
ZelmaSedano <55029831+ZelmaSedano@users.noreply.github.com>
2025-11-19 09:45:56 -0600

    Implement function to get max value from dictionary
    Added a function to retrieve the maximum value from a dictionary.

---
+++ get_max_value_dictionary.py
@@ -0,0 +1,5 @@
+# create a code challenge solution that gets the maximum value of a key from a dictionary
+def max_key_dictionary(dictionary):
+    return max(dictionary.values())
+
+print(max_key_dictionary({'a':1, 'b':2, 'c':3}))
